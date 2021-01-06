4c1c96092488bd8797f1a85083e344c667bda61b
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:38:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken? board, index
+  if board[index] == " "
+    puts "false"
+  elsif board[index] =! " "
+    puts "true"
+  end
+end
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+position_taken?(board, 4)
