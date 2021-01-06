87a538425c77d1533faa4c4d4f55afd70aa49297
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:46:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken? (board, index)
   if board[index] != "X" or board[index] != "O"
     return false
-  elsif board[index] == "X" or board[index] != "O"
+  elsif board[index] == "X" or board[index] == "O"
     return true
   end
 end
