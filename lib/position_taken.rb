550b3336eacff93906b89f35091ba9f7f3016ad1
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:51:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 def position_taken? (board, index)
   puts board[index]
-  binding.pry
   if board[index] != "X" and board[index] != "O"
     return false
   elsif board[index] == "X" or board[index] == "O"
