cc9bc7287b0ca8a92e2d13f94c329b7dd48ce854
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:47:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
+  puts board[index]
   if board[index] != "X" or board[index] != "O"
     return false
   elsif board[index] == "X" or board[index] == "O"
