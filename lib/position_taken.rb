20495619cb7754055bd454591d3176612edac4b6
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:37:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? board, index
+<<<<<<< HEAD
   board[index] == " " || board[index] == "" || board[index] == nil ? false : true
 end
 
@@ -7,3 +8,13 @@ end
   
 
 
+=======
+  if board[index] == " "
+    puts "false"
+  elsif board[index] =! " "
+    puts "true"
+  end
+end
+boa = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+position_taken?(boa, 4)
+>>>>>>> 51af9c4f2bd2dec823d0fc8955150f3410bbe82f
