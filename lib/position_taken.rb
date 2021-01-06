0d918a3f58260e831ac9ef16d4da5ba28fea5823
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 15:50:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
-require binding.pry
+require 'pry'
 
 def position_taken? (board, index)
   puts board[index]
-  pry
+  binding.pry
   if board[index] != "X" and board[index] != "O"
     return false
   elsif board[index] == "X" or board[index] == "O"
